077920de8adf51e9876f6ab51678cb19b32c6445
Ruth Petrie <rpetrie@host595.jc.rl.ac.uk>
2021-03-09 14:29:32 +0000

    assert historical in dataset release

--- analysis/write_json_template.py
+++ analysis/write_json_template.py
@@ -5,56 +5,68 @@ import json
 import argparse
 import logging
 from datetime import datetime as dt
-logging.basicConfig(format='[%(levelname)s]:%(message)s', level=logging.INFO)
+
+FILENAME_CACHE = '../Catalogs/fnames_for_json_template.json'
+RELEASE_IDS_AND_PIDS = '../Catalogs/dataset-ids-pids_release2_202002_2.csv'
+OFILE = 'test-template.json'
+
+
+def setup():
+    logging.basicConfig(format='[%(levelname)s]:%(message)s', level=logging.INFO)
+    if os.path.exists(OFILE):
+        os.remove(OFILE)
+
 
 def main():
     # Read in data
-    with open('../Catalogs/release2_file_pids.json') as jsn:
+    with open(FILENAME_CACHE) as jsn:
         data = json.load(jsn)
 
     dataset_pids = {}
-    with open('../Catalogs/dataset-ids-pids_release2_202002.csv') as r:
+    with open(RELEASE_IDS_AND_PIDS) as r:
         for line in r:
             id, pid = line.split(',')[0], line.split(',')[1].strip()
             dataset_pids[id] = pid
 
     # Set up json header
     jout = {}
-    jout["header"] = {"application:": '', 'Author': '', 'Institution': '', 'Date': '', 'version': ''}
-    jout["results"] = {}
+    jout["header"] = {"application:": '', 'Author': '', 'Institution': '', 'Date': '', 'version': '', 'QC_template': 'version 2: 2021-03-05'}
     jout["datasets"] = {}
 
     for dsid, files in list(data.items()):
-
         if "piControl" in dsid or "amip" in dsid:
-            print(f'skipping ds {dsid}')
+            logging.debug(f'skipping ds in piControl or amip: {dsid}')
             continue
 
         dspid = dataset_pids[dsid]
-        logging.debug(dsid, dspid)
-        json_ds_qc = {'error_severity': 'na', 'error_message': 'na'}
-        ds_qcStatus = ""
+        logging.debug(f'ID: {dsid}; PID: {dspid}')
 
-        json_files ={}
-        for f in files:
-            for fpid, fname in f.items():
-                json_files[fpid] = {'filename': fname,
-                                    'qc_status': '',
-                                    'error_severity': '',
-                                    'error_message': ''
-                                    }
-                logging.debug(fpid, fname)
+        json_files = {}
+        for fname, fpid in files.items():
+            logging.debug(f'FILENAME: {fname}, FILE_PID: {fpid}')
 
-        jout["datasets"][dspid] = {'dset_id': dsid,
-                                   'qc_status': ds_qcStatus,
-                                   'dataset_qc': json_ds_qc,
-                                   'files': json_files
-                                   }
+            # Set file level info
+            json_files[fpid] = {'filename': fname,
+                                'file_qc_status': '',
+                                'file_error_severity': '',
+                                'file_error_message': ''
+                                }
+
+        # Set dataset level details
+        jout["datasets"][dspid] = {
+            'dset_id': dsid,
+            'dset_qc_status': '',
+            'dset_error_severity': '',
+            'dset_error_message': '',
+            'files': json_files
+        }
+
     # Output JSON
     json_obj = json.dumps(jout, indent=4)
-    with open('QC_template.json', "a+") as o:
+    with open(OFILE, "a+") as o:
         o.write(json_obj)
 
 
 if __name__ == "__main__":
+    setup()
     main()
